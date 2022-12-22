ac3ccaec13525f630299ec0fa32abbd81f8ee4ad
Joseph Akinde-peters <apjoex@gmail.com>
2022-12-22 16:18:51 +0200

    Initial Commit

---
+++ Flick/FlickApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickApp.swift
+//  Flick
+//
+//  Created by JOSEPH AKINDE-PETERS on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
