2c662f1c57ef6ee4c233ada5d9c46274dff7fa5b
Joseph Akinde-peters <apjoex@gmail.com>
2022-12-27 17:03:30 +0200

    Basic flick row and UI done

--- Flick/Views/ContentView.swift
+++ Flick/Views/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var selectedFlick: Flick?
+    @State private var searchQuery: String = ""
+    
+    private var flicks: [Flick] {
+        let allFlicks = Flick.sample
+        if !searchQuery.isEmpty {
+            return allFlicks.filter { flick in
+                flick.title.localizedCaseInsensitiveContains(searchQuery) ||
+                flick.plot.localizedCaseInsensitiveContains(searchQuery)
+            }
+        } else {
+            return allFlicks
         }
-        .padding()
+    }
+    
+    var body: some View {
+        NavigationSplitView {
+            List(flicks, selection: $selectedFlick) { flick in
+                NavigationLink(value: flick) {
+                    FlickRow(flick: flick)
+                        .swipeActions(edge: .trailing) {
+                            Button(role: .destructive) {
+                                //remove from favs
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                                    .labelStyle(IconOnlyLabelStyle())
+                            }
+                            
+                        }
+                }
+            }
+            .listStyle(.sidebar)
+            .navigationTitle("Flicks")
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Label("Add flick", systemImage: "plus.circle.fill")
+                            .labelStyle(TitleAndIconLabelStyle())
+                    }
+                }
+            })
+            .searchable(text: $searchQuery, prompt: "Search")
+        } detail: {
+            NavigationStack {
+                Text("Details gooes here for \(selectedFlick?.title ?? "")")
+                    .navigationTitle(selectedFlick?.title ?? "")
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+        }
+        
     }
 }
 
--- Flick/Views/FlickRow.swift
+++ Flick/Views/FlickRow.swift
@@ -12,19 +12,34 @@ struct FlickRow: View {
     
     var body: some View {
         HStack (spacing: 10) {
-            Image(systemName: "play.tv.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50)
+            AsyncImage(url: URL(string: flick.poster)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+            } placeholder: {
+                Color.gray
+            }
+            .frame(width: 70)
+            .cornerRadius(6.0)
             
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 5) {
                 Text(flick.title)
-                    .font(.title2)
+                    .font(.title3)
                     .bold()
                 
-                Text(flick.details)
+                Text(flick.plot)
+                    .font(.body)
                     .multilineTextAlignment(.leading)
                     .lineLimit(2)
+                
+                HStack {
+                    Image(systemName: "star.fill")
+                        .imageScale(.small)
+                        .symbolRenderingMode(.multicolor)
+                    
+                    Text(String(describing: flick.rating))
+                }
             }
         }
         .padding()
@@ -33,7 +48,7 @@ struct FlickRow: View {
 
 struct FlickRow_Previews: PreviewProvider {
     static var previews: some View {
-        let flick = Flick(id: 3, title: "Flick title", details: "Flick details")
+        let flick = Flick.sample.first!
         FlickRow(flick: flick)
     }
 }
